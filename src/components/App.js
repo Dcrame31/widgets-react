edfcc7cd5e91ed2ae9aa9834d5bb6805978f2c6c
Diane Crame <dcrame@icloud.com>
2021-01-14 18:03:26 -0800

    add items array and semantic ui css

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Accordion from './Accordion';
 
 // class App extends React.Component {
 //     render() {
@@ -12,6 +13,25 @@ import React from 'react';
 
 // export default App;
 
+const items = [
+    {
+        title: 'What is React',
+        content: 'React is a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library amongst engineers'
+    },
+    {
+        title: 'How do you use React?',
+        content: 'You use React by creating components'
+    }
+]
+
 export default () => {
-    return <h1>Widgets App</h1>
+    return (
+        <div>
+            <Accordion items={items} />
+        </div>
+    )
 };
