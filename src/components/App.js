0f058c3e05f554e82dc69a504cbded9860e12e02
Diane Crame <dcrame@icloud.com>
2021-01-14 17:50:17 -0800

    set up index

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// class App extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 App
+//             </div>
+//         )
+//     }
+// };
+
+// export default App;
+
+export default () => {
+    return <h1>Widgets App</h1>
+};
