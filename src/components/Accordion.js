52e59139dd06eded92b3bdb0bd4d42be6e88fde2
Diane Crame <dcrame@icloud.com>
2021-01-16 14:22:57 -0800

    update div className when active

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Accordion = ({ items }) => {
-    const renderedItems = items.map(item => {
-        return <div key={item.title}>
-            <div className="title active">
+
+    const [activeIndex, setActiveIndex] = useState(null); 
+
+    const onTitleClick = (index) => {
+        setActiveIndex(index)
+    }
+
+    const renderedItems = items.map((item, index) => {
+        const active = index === activeIndex ? 'active' : '';
+        return (
+        <React.Fragment key={item.title}>
+            <div 
+                onClick={() => onTitleClick(index)}
+                className={`title ${active}`}>
                 <i className="dropdown icon"></i>
                 {item.title}
             </div>
-            <div className="content active">
+            <div className={`content ${active}`}>
                 <p>{item.content}</p>
             </div>
-        </div>
+        </React.Fragment>
+        )
     })
     return (
         <div className="ui styled accordion">
