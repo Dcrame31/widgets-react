5b7b958309bd3a70f0d8774072c3be7b09b687b5
Diane Crame <dcrame@icloud.com>
2021-01-16 15:59:34 -0800

    install axios and apply to useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
 
     const [ term, setTerm ] = useState('');
     const [ results, setResults ] = useState([]);
 
+    useEffect(() => {
+        const search = async () => {
+            await axios.get('https://en.wikipedia.org/w/api/php', {
+            params: {
+                action: 'query',
+                list: 'search',
+                origin: '*',
+                format: 'json',
+                srsearch: term,
+            },
+        });
+        };
+        
+        search();
+    }, [term]);
+
+    const onSubmitClick = (term) => {
+        setResults(term)
+    }
+
      return (
         <div>
-            <div 
-                onSubmit={console.log(term)}
-                className="ui form">
+            <div className="ui form">
                 <div className="field">
                     <label>Search</label>
                     <input 
