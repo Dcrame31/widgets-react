d652984f3d88090e1fd56a9c5bb0e051d69135b2
Diane Crame <dcrame@icloud.com>
2021-01-17 18:03:34 -0800

    add dropdown component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './Accordion';
 import Search from './Search';
+import Dropdown from './Dropdown';
 
 // class App extends React.Component {
 //     render() {
@@ -33,7 +34,8 @@ export default () => {
     return (
         <div className="ui container">
             {/* <Accordion items={items} /> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown />
         </div>
     )
 };
