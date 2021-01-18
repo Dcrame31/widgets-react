2c623cf4ed2a1a9ef6eb007dce98a3bebad2f62a
Diane Crame <dcrame@icloud.com>
2021-01-17 17:59:58 -0800

    add debouncedTerm state to fix bug

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,18 @@ import axios from 'axios';
 const Search = () => {
     
     const [ term, setTerm ] = useState('Plants');
+    const [ debouncedTerm, setDebouncedTerm ] = useState(term);
     const [ results, setResults ] = useState([]);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -15,28 +25,46 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term,
+                    srsearch: debouncedTerm,
                 },
             });
         setResults(data.query.search);
         };
+        if ( debouncedTerm ) {
+            search();
+        }
+    }, [debouncedTerm]);
 
-        if (term && !results){
-            search()
-        } else {
-            const timeoutId = setTimeout(()=>{
-                if(term){
-                    search();
-                }
-            }, 500)
+    // useEffect(() => {
+    //     const search = async () => {
+    //         const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+    //             params: {
+    //                 action: 'query',
+    //                 list: 'search',
+    //                 origin: '*',
+    //                 format: 'json',
+    //                 srsearch: term,
+    //             },
+    //         });
+    //     setResults(data.query.search);
+    //     };
+
+    //     if (term && !results.length){
+    //         search()
+    //     } else {
+    //         const timeoutId = setTimeout(()=>{
+    //             if(term){
+    //                 search();
+    //             }
+    //         }, 500)
     
-            return () => {
-                clearTimeout(timeoutId)
-            };
-        };
+    //         return () => {
+    //             clearTimeout(timeoutId)
+    //         };
+    //     };
         
 
-    }, [term]);
+    // }, [term, results.length]);
 
 
     const renderedResults = results.map((result, index) => {
@@ -47,6 +75,7 @@ const Search = () => {
                         className="ui button"
                         href={`https://en.wikipedia.org?curid=${result.pageid}`}
                         target="_blank"
+                        rel="noreferrer"
                         >
                         Go
                     </a>
