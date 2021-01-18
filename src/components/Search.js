1c1eb6c9d24863cbd8d4e92dd98a9df8646619d3
Diane Crame <dcrame@icloud.com>
2021-01-17 17:24:14 -0800

    add setTimeout to control api call frequency

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,29 +2,70 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Search = () => {
-
-    const [ term, setTerm ] = useState('');
+    
+    const [ term, setTerm ] = useState('Plants');
     const [ results, setResults ] = useState([]);
+
 
     useEffect(() => {
         const search = async () => {
-            await axios.get('https://en.wikipedia.org/w/api/php', {
-            params: {
-                action: 'query',
-                list: 'search',
-                origin: '*',
-                format: 'json',
-                srsearch: term,
-            },
-        });
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term,
+                },
+            });
+        setResults(data.query.search);
+        };
+
+        if (term && !results){
+            search()
+        } else {
+            const timeoutId = setTimeout(()=>{
+                if(term){
+                    search();
+                }
+            }, 500)
+    
+            return () => {
+                clearTimeout(timeoutId)
+            };
         };
         
-        search();
+
     }, [term]);
 
-    const onSubmitClick = (term) => {
-        setResults(term)
-    }
+
+    const renderedResults = results.map((result, index) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                        target="_blank"
+                        >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <div>
+                        <span dangerouslySetInnerHTML={{__html: result.snippet}}></span>
+                    </div>
+                </div>
+            </div>
+        )
+    })
+
+    // const onSubmitClick = (term) => {
+    //     setResults(term)
+    // }
 
      return (
         <div>
@@ -38,6 +79,7 @@ const Search = () => {
                         type="text" />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     )
 };
